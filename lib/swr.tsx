0f032c01cc36062235e5bca6c3bd538b68a255a9
Tino Tonitini <georgepike0@gmail.com>
2025-07-31 19:32:03 -0400

    Add SWR setup and provider

---
+++ lib/swr.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { SWRConfig } from 'swr'
+
+export const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+export function SWRProvider({ children }: { children: React.ReactNode }) {
+  return <SWRConfig value={{ fetcher }}>{children}</SWRConfig>
+}
